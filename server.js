b5f3083a3a13821519b5f38149e241f93deca8da
tmc-yukihide-ohta <yukihide_ohta@mail.toyota.co.jp>
2023-06-08 10:09:36 +0900

    server修正中2_render対応

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const dotenv = require('dotenv');
 const knex = require("./db/knex");
 const port = process.env.PORT || 8080
 const app = express();
+const cors = require("cors");
 
 // body-parserミドルウェアを使用してリクエストのボディをパース
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// const cors = require("cors");
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
     next();
 });
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server is running ${port} !`);
